0b1a654c1e2b1fbf0c45efff17494ac759fdbcd8
Evan Rost <11428231+edubz@users.noreply.github.com>
2021-05-02 11:24:04 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,1 @@
-let hamburger = document.getElementById("hamburger");
-let nav = document.getElementById("nav");
 
-hamburger.onclick = () => {
-  hamburger.classList.toggle("open");
-  nav.classList.toggle("open");
-  console.log(nav);
-};
