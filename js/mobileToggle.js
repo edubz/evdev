767759a2a0b0001c967c1e4f6defc7f8e1e76449
evan <ev4n.rost@gmail.com>
2021-05-05 16:40:14 -0700

    created menu toggle animation

--- js/mobileToggle.js
+++ js/mobileToggle.js
@@ -1,7 +1,12 @@
 
 const hamburger = document.getElementById('hamburger');
 const nav = document.querySelector('nav');
+const burgerSvg = document.querySelectorAll('rect')
+console.log(burgerSvg)
 
 hamburger.onclick = function(){
     nav.classList.toggle('menuToggle')
+    burgerSvg[0].classList.toggle('top');
+    burgerSvg[1].classList.toggle('middle');
+    burgerSvg[2].classList.toggle('bottom')
 }
