52cf1bffc41e70c5e1984c04c013d85941e2dfd8
evan <ev4n.rost@gmail.com>
2021-05-05 17:07:29 -0700

    remove nav classes on window resize

--- js/mobileToggle.js
+++ js/mobileToggle.js
@@ -2,7 +2,19 @@
 const hamburger = document.getElementById('hamburger');
 const nav = document.querySelector('nav');
 const burgerSvg = document.querySelectorAll('rect')
-console.log(burgerSvg)
+
+console.log(nav.className)
+
+window.onresize = function(event) {
+    if (window.innerWidth >= 600 && nav.className == "menuToggle") {
+        nav.classList.remove('menuToggle');
+        burgerSvg[0].classList.remove('top');
+        burgerSvg[1].classList.remove('middle');
+        burgerSvg[2].classList.remove('bottom')
+      }
+}
+
+  
 
 hamburger.onclick = function(){
     nav.classList.toggle('menuToggle')
